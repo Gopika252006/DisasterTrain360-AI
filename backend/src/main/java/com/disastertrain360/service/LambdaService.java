33513922cadf807ad9406e5815c8f4b1fc7291d3
Gopika252006 <gopika252006@gmail.com>
2026-06-26 22:56:27 +0530

    Integrated AWS Lambda invocation

--- backend/src/main/java/com/disastertrain360/service/LambdaService.java
+++ backend/src/main/java/com/disastertrain360/service/LambdaService.java
@@ -1,11 +1,29 @@
 package com.disastertrain360.service;
 
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.services.lambda.LambdaClient;
+import software.amazon.awssdk.services.lambda.model.InvokeRequest;
+import software.amazon.awssdk.services.lambda.model.InvokeResponse;
 
 @Service
 public class LambdaService {
 
+    private static final String FUNCTION_NAME = "disastertrain360-training-processor";
+
     public void invokeTrainingLambda(String trainingName) {
-        System.out.println("Lambda Invoked for: " + trainingName);
+
+        LambdaClient lambdaClient = LambdaClient.create();
+
+        String payload = "{ \"trainingName\": \"" + trainingName + "\" }";
+
+        InvokeRequest request = InvokeRequest.builder()
+                .functionName(FUNCTION_NAME)
+                .payload(SdkBytes.fromUtf8String(payload))
+                .build();
+
+        InvokeResponse response = lambdaClient.invoke(request);
+
+        System.out.println("Lambda Status Code : " + response.statusCode());
     }
 }
