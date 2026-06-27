2cd98f5e8b8e1d9a89ecc860bdfd5fe31ccdf0f7
Gopika252006 <gopika252006@gmail.com>
2026-06-27 11:59:20 +0530

    Updated Lambda configuration and removed AWS credentials

--- backend/src/main/java/com/disastertrain360/service/LambdaService.java
+++ backend/src/main/java/com/disastertrain360/service/LambdaService.java
@@ -1,7 +1,12 @@
 package com.disastertrain360.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.lambda.LambdaClient;
 import software.amazon.awssdk.services.lambda.model.InvokeRequest;
 import software.amazon.awssdk.services.lambda.model.InvokeResponse;
@@ -9,21 +14,40 @@ import software.amazon.awssdk.services.lambda.model.InvokeResponse;
 @Service
 public class LambdaService {
 
-    private static final String FUNCTION_NAME = "disastertrain360-training-processor";
+    @Value("${aws.lambda.region}")
+    private String region;
+
+    @Value("${aws.lambda.access-key}")
+    private String accessKey;
+
+    @Value("${aws.lambda.secret-key}")
+    private String secretKey;
+
+    @Value("${app.aws.lambda.function-name}")
+    private String functionName;
 
     public void invokeTrainingLambda(String trainingName) {
 
-        LambdaClient lambdaClient = LambdaClient.create();
+        AwsBasicCredentials credentials =
+                AwsBasicCredentials.create(accessKey, secretKey);
+
+        LambdaClient lambdaClient = LambdaClient.builder()
+                .region(Region.of(region))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(credentials))
+                .build();
 
         String payload = "{ \"trainingName\": \"" + trainingName + "\" }";
 
         InvokeRequest request = InvokeRequest.builder()
-                .functionName(FUNCTION_NAME)
+                .functionName(functionName)
                 .payload(SdkBytes.fromUtf8String(payload))
                 .build();
 
         InvokeResponse response = lambdaClient.invoke(request);
 
         System.out.println("Lambda Status Code : " + response.statusCode());
+
+        lambdaClient.close();
     }
 }
